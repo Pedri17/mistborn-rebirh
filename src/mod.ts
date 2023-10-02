8fd1db8daf0846930e9a8e7a05d3e6a7d5da775d
Pedri17 <pedrojalda@gmail.com>
2023-10-02 14:20:06 +0200

    fixed features

--- src/mod.ts
+++ src/mod.ts
@@ -2,9 +2,8 @@ import { ISCFeature, upgradeMod } from "isaacscript-common";
 
 const modVanilla = RegisterMod("MistbornRebirthTS", 1);
 const features = [
-  ISCFeature.PICKUP_INDEX_CREATION,
-  ISCFeature.CUSTOM_PICKUPS,
   ISCFeature.SAVE_DATA_MANAGER,
   ISCFeature.PICKUP_INDEX_CREATION,
+  ISCFeature.CUSTOM_PICKUPS,
 ] as const;
 export const mod = upgradeMod(modVanilla, features);
